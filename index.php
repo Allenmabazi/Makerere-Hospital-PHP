c228b54a9f1a798ad5b2cd00c183a43ae9da421f
AllenMbabazi <50418190+Allenmabazi@users.noreply.github.com>
2021-01-10 19:19:02 +0300

    First Commit
    Php index page

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="mak.css">
+    <title>Makerere Hospital</title>
+</head>
+<body>
+<?php
+include 'header.php';
+?>
+<div class="Bod">
+  <div class="left">
+<button>VISIT NOW...</button>
+</div>
+<div class="right2">
+<img src="mic.jpg" alt="">
+<br>
+<h4>Drugs & Medications A-Z</h4>
+<br><br>
+<h5>Better Services</h5>
+<img src="child-ward.jpg" alt="">
+</div>
+
+
+</div>
+
+
+<?php
+include 'footer.php';
+?>
+</body>
+</html>
